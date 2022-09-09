29db9a9ae5fc91084db7932f739f1e7822439c0f
GaHyeon <parka0017@gmail.com>
2022-09-09 22:31:18 +0900

    Initial Setting 수정 (#3)
    * [wip] repository datasource 추가 (#2)
    
    * stardust initial setting
    
    * [wip] repository datasource 추가
    
    * build : build setting 수정
    
    * refactor : initial setting 수정

--- src/main/java/ssumc/stardust/src/controller/UserController.java
+++ src/main/java/ssumc/stardust/src/controller/UserController.java
@@ -1,8 +1,9 @@
-package ssumc.stardust.src.user;
+package ssumc.stardust.src.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ssumc.stardust.src.service.UserService;
 import ssumc.stardust.utils.JwtService;
 
 @RestController
--- src/main/java/ssumc/stardust/src/user/model/PostSignUpReq.java
+++
@@ -1,9 +0,0 @@
-package ssumc.stardust.src.user.model;
-
-import lombok.Data;
-
-@Data
-public class PostSignUpReq {
-    private String studentId;
-    private String phoneNum;
-}
