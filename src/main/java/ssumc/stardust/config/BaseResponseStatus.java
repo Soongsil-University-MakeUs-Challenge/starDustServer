0f9062781d17a4fc1dcb987e4406a9501a09324c
GaHyeon <parka0017@gmail.com>
2022-09-10 22:54:49 +0900

    doc : http 상태 코드 수정 (#4)

--- src/main/java/ssumc/stardust/config/BaseResponseStatus.java
+++ src/main/java/ssumc/stardust/config/BaseResponseStatus.java
@@ -5,31 +5,28 @@ import lombok.Getter;
 @Getter
 public enum BaseResponseStatus {
     /**
-     * 1000 : 요청 성공
+     * 2XX : 요청 성공
      */
-    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    SUCCESS(true, 200, "요청에 성공하였습니다."),
 
     /**
-     * 2000 : Request 오류
+     *4XX  : Request 오류
      */
     // Common
-    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
-    EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
-    INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
-    INVALID_USER_JWT(false, 2003, "권한이 없는 유저의 접근입니다."),
+    REQUEST_ERROR(false, 400, "입력값을 확인해주세요."),
+    EMPTY_JWT(false, 401, "JWT를 입력해주세요."),
+    INVALID_JWT(false, 402, "유효하지 않은 JWT입니다."),
+    INVALID_USER_JWT(false, 403, "권한이 없는 유저의 접근입니다."),
+
 
     /**
-     * 3000 : Response 오류
+     * 5XX : Database, Server 오류
      */
+    SERVER_ERROR(false, 500, "서버와의 연결에 실패하였습니다."),
+    DATABASE_ERROR(false, 501, "데이터베이스 연결에 실패하였습니다."),
 
-    /**
-     * 4000 : Database, Server 오류
-     */
-    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
-    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
-
-    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
-    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+    PASSWORD_ENCRYPTION_ERROR(false, 502, "비밀번호 암호화에 실패하였습니다."),
+    PASSWORD_DECRYPTION_ERROR(false, 503, "비밀번호 복호화에 실패하였습니다.");
 
     private final boolean isSuccess;
     private final int code;
