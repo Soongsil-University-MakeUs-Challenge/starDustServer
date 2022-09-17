f9abd359596048b6612eea19b4e53e3740474e81
GaHyeon <parka0017@gmail.com>
2022-09-17 21:42:02 +0900

    build : jdbc로 수정, 기본 파일 명 수정 (#7)

--- src/main/java/ssumc/stardust/src/controller/UserController.java
+++ src/main/java/ssumc/stardust/src/controller/UserController.java
@@ -3,13 +3,13 @@ package ssumc.stardust.src.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ssumc.stardust.src.service.MemberService;
+import ssumc.stardust.src.service.UserService;
 import ssumc.stardust.utils.JwtService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/members")
-public class MemberController {
-    private final MemberService memberService;
+@RequestMapping("/users")
+public class UserController {
+    private final UserService UserService;
     private final JwtService jwtService;
 }
---
+++ src/main/java/ssumc/stardust/src/domain/User.java
@@ -0,0 +1,8 @@
+package ssumc.stardust.src.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+}
