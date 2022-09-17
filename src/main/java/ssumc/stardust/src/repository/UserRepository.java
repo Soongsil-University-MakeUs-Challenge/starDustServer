85c117c9709e1c6738b33793930f8d6f56b53d4f
gahyeonn <parka0017@gmail.com>
2022-09-17 21:40:35 +0900

    build : jdbc로 수정, 기본 파일 명 수정

---
+++ src/main/java/ssumc/stardust/src/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package ssumc.stardust.src.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+}
