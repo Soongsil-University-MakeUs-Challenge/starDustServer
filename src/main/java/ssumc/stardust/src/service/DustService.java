7f1168e2babafd40c222c179052a705a204781a4
GaHyeon <parka0017@gmail.com>
2022-09-19 16:30:11 +0900

    feat : 먼지 좌표 전송 기능 구현 (#8)
    * build : jdbc로 수정, 기본 파일 명 수정
    
    * build : validation 추가
    
    * feat : 먼지 좌표 전송 기능 구현
    
    * fix : 토큰 만료기간 수정
    
    * fix : 유저 역할 확인 validation 추가

---
+++ src/main/java/ssumc/stardust/src/service/DustService.java
@@ -0,0 +1,41 @@
+package ssumc.stardust.src.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ssumc.stardust.config.BaseException;
+import ssumc.stardust.src.domain.DustLocationDto;
+import ssumc.stardust.src.repository.DustRepository;
+
+import static ssumc.stardust.config.BaseResponseStatus.DATABASE_ERROR;
+import static ssumc.stardust.config.BaseResponseStatus.INVALID_USER_JWT;
+
+@Service
+@RequiredArgsConstructor
+public class DustService {
+
+    private final DustRepository dustRepository;
+
+    /**
+    먼지 위치 정보 업데이트
+     */
+    public String setLocation (int userId, DustLocationDto dustLocationDto) throws BaseException {
+
+        if (!dustRepository.checkUserRole(userId).equals("STAFF")) {
+            throw new BaseException(INVALID_USER_JWT);
+        }
+
+        try {
+            int result = dustRepository.setLocation(userId, dustLocationDto);
+            if (result == 0) {
+                throw new BaseException(DATABASE_ERROR);
+            }
+            return "성공적으로 업데이트 했습니다.";
+
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+
+
+}
