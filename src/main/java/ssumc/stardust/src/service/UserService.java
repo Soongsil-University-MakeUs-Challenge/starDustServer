7076da440e5361a5dab244a6580cdcbb30de80e4
GaHyeon <parka0017@gmail.com>
2022-09-25 00:58:30 +0900

    fix : 타이머 시작 기능 Validation 추가 (#18)
    * style : 코드 정렬 수정
    
    * fix : 타이머 시작 기능 validation 추가

--- src/main/java/ssumc/stardust/src/service/UserService.java
+++ src/main/java/ssumc/stardust/src/service/UserService.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service;
 import ssumc.stardust.config.BaseException;
 import ssumc.stardust.src.repository.UserRepository;
 
-import static ssumc.stardust.config.BaseResponseStatus.DATABASE_ERROR;
-import static ssumc.stardust.config.BaseResponseStatus.INVALID_USER_JWT;
+import static ssumc.stardust.config.BaseResponseStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +16,14 @@ public class UserService {
      * 타이머 시작
      */
     public String startTimer(int userId) throws BaseException {
+        if (userRepository.checkDuplication(userId) == 1) { // 이미 타이머가 시작된 경우 => 데이터가 이미 존재하는 경우
+           throw new BaseException(DUPLICATED_DATA);
+        }
+
         if (userRepository.checkUserRole(userId) == 0) { // 유저 역할이 USER인 경우만 타이머 시작
             throw new BaseException(INVALID_USER_JWT);
         }
+
         try {
             if (userRepository.insertStartTime(userId) == 0) {
                 throw new BaseException(DATABASE_ERROR);
